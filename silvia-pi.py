d07e9058a402a556899482da108965d8b92205c9
Petar Stankovic <petar@hyperchicken.com>
2022-03-11 01:23:18 +1100

    anim

--- silvia-pi.py
+++ silvia-pi.py
@@ -349,7 +349,7 @@ def oled_display(dummy, state):
 
   def coffee_anim(x, y, frame):
     draw.rectangle((0+x, 7+y, 9+x, 13+y), outline=0, fill=255)
-    draw.rectangle((1+x, 14+y, 8+x, 16+y), outline=0, fill=255)
+    draw.rectangle((1+x, 13+y, 8+x, 15+y), outline=0, fill=255)
     draw.point((9+x, 9+y), fill=255)
     draw.point((10+x, 9+y), fill=255)
     draw.point((10+x, 10+y), fill=255)
